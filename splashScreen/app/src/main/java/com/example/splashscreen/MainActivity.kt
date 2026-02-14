e97f71f77d180075d00467dadb83878b15349cde
Amna5629 <amnayoosuf123@gmail.com>
2026-02-14 15:52:51 +0530

    initial commit splash screen

---
+++ splashScreen/app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.splashscreen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        // MUST be first
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+}
